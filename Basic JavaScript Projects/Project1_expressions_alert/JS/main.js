3082e4892b5bee45fc1980c5c15166578a86ec56
keraberas <46618649+keraberase@users.noreply.github.com>
2025-02-15 15:03:32 +0000

    Assign multiple variables in one statement

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,28 @@
-// window.alert("Hello world");
+// Display a pop-up alert with a message
+window.alert("Hello world");
+
+// Output text to the webpage
 document.write('Hello, world!');
+
+// Declare a variable and display its value
+var x = "Hello, world!";
+document.write(x);
+
+// Use let to store a string and display it in an alert
+let myString = "Hello, world!";
+window.alert(myString);
+
+// Escape quotation marks in a string
+let myEscape = "John said, \"I can’t wait to see you!\"";
+document.write(myEscape);
+
+// Concatenate strings and display the result
+let myConcat = "I am learning a lot" + " from this book!";
+document.write(myConcat);
+
+// Concatenate strings and display the result
+let myMessage = "Hello, " + "this is a " + "concatenated string.";
+document.write(myMessage);
+// Assign multiple variables in one statement
+let name = "John", age = 30, city = "New York";
+document.write(name);
