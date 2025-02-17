8aae20950a9f9a128cfc6e95b90d670f359af247
keraberas <46618649+keraberase@users.noreply.github.com>
2025-02-17 18:17:06 +0000

    Add comments

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,5 @@
 // Display a pop-up alert with a message
-window.alert("Hello world");
+// window.alert("Hello world");
 
 // Output text to the webpage
 document.write('Hello, world!');
@@ -9,8 +9,8 @@ var x = "Hello, world!";
 document.write(x);
 
 // Use let to store a string and display it in an alert
-let myString = "Hello, world!";
-window.alert(myString);
+// let myString = "Hello, world!";
+// window.alert(myString);
 
 // Escape quotation marks in a string
 let myEscape = "John said, \"I can’t wait to see you!\"";
@@ -26,6 +26,18 @@ document.write(myMessage);
 // Assign multiple variables in one statement
 let name = "John", age = 30, city = "New York";
 document.write(name);
+
+
+// Declare two string variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate the two strings and display them on the webpage
+document.write(Sent1 + Sent2);
+
+
+// Display an alert message
+window.alert("Hello! This is a JavaScript alert.");
 
 // Write an expression
 document.write(5 + 5);
