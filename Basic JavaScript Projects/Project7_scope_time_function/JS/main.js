51bb2357624571b28aab7a5fda401f16d6b67138
keraberas <46618649+keraberase@users.noreply.github.com>
2025-03-10 15:44:13 +0000

    save

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,34 @@
+var a = 50;
+function addNumbers() {
+    document.write(20 + a + "<br>");
+}
+function addNumbers2() {
+    document.write(a + 1000+"<br>");
+}
+addNumbers();
+addNumbers2();
+
+function addNumbers3() {
+
+    var b = 100;
+    document.write(20 + b + "<br>");
+}
+function addNumbers4() {
+    document.write(b + 1000);
+}
+
+addNumbers3();
+addNumbers4();
+
+function addNumbers5() {
+
+    var c = 100;
+    console.log(20 + c);
+}
+function addNumbers6() {
+    console.log(c + 1000);
+}
+
+addNumbers5();
+addNumbers6();
+
