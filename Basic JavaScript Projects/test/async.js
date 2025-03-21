96e73a10a966ccf7dda086f80ad5f92832d81213
keraberas <46618649+keraberase@users.noreply.github.com>
2025-03-21 12:59:05 +0000

    save

---
+++ Basic JavaScript Projects/test/async.js
@@ -0,0 +1,6 @@
+alert (document.getElementById("p1").innerHTML);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let heading = document.getElementById("myHeading");
+    heading.textContent = "JavaScript good to go!";
+});
